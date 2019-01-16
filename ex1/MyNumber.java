53841c6fa219be5e828242ca41c7cc5e47d7630e
5710110568 <5710110568@psu.ac.th>
2019-01-16 22:45:30 +0700

    first commit

---
+++ ex1/MyNumber.java
@@ -0,0 +1,27 @@
+class Counter{
+    private int val;
+    public void counter(){
+        val=0;
+        }
+    public void inc(){
+        val++;
+    }
+    public void reset(){
+        val=0;
+    }
+    public int getval(){
+        return val;
+    }
+}
+public class MyNumber{
+ public static void main(String args[]){
+     Counter c = new Counter();
+     for(int i=0;i<5;i++){
+         while(c.getval()<10){
+             c.inc();
+             System.out.println(c.getval());
+         }
+         c.reset();
+     }
+ }
+}
