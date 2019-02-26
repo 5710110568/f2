cb038a17dfd16dccd76fb6e6e72c8d991bd91be2
5710110568 <5710110568@psu.ac.th>
2019-02-26 22:14:08 +0700

    รับเลข 2ตัวเเล้วบวกเลข

---
+++ ex6/ScannerAdding.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class ScannerAdding{
+    public static void main(String[] args){
+        Scanner a= new Scanner(System.in);
+      int x=a.nextInt();
+      System.out.println("");
+      int y= a.nextInt();
+      System.out.println("Adding result = "+(x+y));
+
+       
+
+    }
+}
